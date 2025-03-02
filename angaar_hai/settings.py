e1130490143ed0eda078196f85d84f49e04b4c37
Divyanshu Khandelwal <khandelwalprinci1@gmail.com>
2025-03-02 14:06:14 +0530

    updated footer and articles page

--- angaar_hai/settings.py
+++ angaar_hai/settings.py
@@ -263,7 +263,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "82930f001@smtp-brevo.com"
+EMAIL_HOST_USER = "82930f002@smtp-brevo.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'noreply@theangaarbatch.in'
 
