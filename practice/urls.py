40a37757f2805a824e099029ca0f60d9244fd1d2
Divyanshu Khandelwal <khandelwalprinci1@gmail.com>
2025-04-10 21:40:25 +0530

    fixed url issue in practice.views

--- practice/urls.py
+++ practice/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from . import views, execution_views
 
 
 
@@ -8,28 +8,14 @@ urlpatterns = [
     path("", views.practice , name="practice"),
     
     path("<slug:slug>/", views.sheet , name="sheet"),
-    
     path("playground", views.playground, name="playground"),
-    path("execute_code", views.execute_code, name="execute_code"),
-    
-    path("run_code/<slug:slug>/", views.run_code, name="run_code"),
-    path('submit_code/<slug:slug>/', views.submit_code, name='submit_code'),
-    path("custom_input/<slug:slug>/", views.custom_input, name="custom_input"),
-    
-    path("problem/<slug:slug>/", views.problem, name="problem"),
-    path("get-driver-code/<int:question_id>/<int:language_id>", views.get_driver_code, name='get_driver_code'),
-    path("my_submissions/<slug:slug>/", views.my_submissions, name="my_submissions"),
     
     path("problem_set", views.problem_set, name="problem_set"),
     path("fetch_questions", views.fetch_questions, name="fetch_questions"),
 
-    path('unlock-hint/<int:question_id>/', views.unlock_hint, name='unlock_hint'),
-    path("fetch_recommended_questions/<slug:slug>", views.fetch_recommended_questions, name="fetch_recommended_questions"),
-
-    
     path('render_next_question_in_sheet/<int:sheet_id>/<int:question_id>/', views.render_next_question_in_sheet, name='render_next_question_in_sheet'),
     # path("submission/<slug:slug>/", views.submission, name="submission"),
-        
+    
     # ============================== QUESTION CRUD =========================
     
     path("add_question", views.add_question, name="student_add_question"),
@@ -38,5 +24,26 @@ urlpatterns = [
     path("test_cases/<slug:slug>/", views.test_cases, name="student_test_cases"),
     path("delete_test_case/<int:id>/", views.delete_test_case, name="student_delete_test_case"),
     path("delete_question/<int:id>/", views.delete_question, name="student_delete_question"),
+
+]
+
+# ==================================== EXECUTION URLS ========================================
+
+urlpatterns += [
+    path("problem/<slug:slug>/", execution_views.problem, name="problem"),
+    path("my_submissions/<slug:slug>/", views.my_submissions, name="my_submissions"),
+    
+    path("run_code/<slug:slug>/", execution_views.run_code, name="run_code"),
+    path("execute_code", execution_views.execute_code, name="execute_code"),
+    
+    # ----- ** -----
+    path('submit_code/<slug:slug>/', execution_views.submit_code, name='submit_code'),
+    # ----- ** -----
+    
+    path("custom_input/<slug:slug>/", execution_views.custom_input, name="custom_input"),
+    
+    path("get-driver-code/<int:question_id>/<int:language_id>", execution_views.get_driver_code, name='get_driver_code'),
+    path('unlock-hint/<int:question_id>/', execution_views.unlock_hint, name='unlock_hint'),
+    path("fetch_recommended_questions/<slug:slug>", execution_views.fetch_recommended_questions, name="fetch_recommended_questions"),
 
 ]
