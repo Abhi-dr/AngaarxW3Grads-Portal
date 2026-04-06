63c74ee49c3f67080b956874d146b6c45b74b1a3
User <user@example.com>
2026-04-06 12:06:25 +0530

    corrected search bar and pagination on admin

--- administration/api/views/sheet_api.py
+++ administration/api/views/sheet_api.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from rest_framework.filters import SearchFilter
 
 from practice.models import Sheet
 from administration.api.serializers.sheet_serializers import SheetAdminSerializer
@@ -8,12 +9,15 @@ from administration.api.permissions import IsAdministratorOrInstructor
 class SheetAdminViewSet(viewsets.ModelViewSet):
     """
     Unified API for Administrators and Instructors to manage Sheets.
+    Supports ?search= for name/description/type and ?page= for pagination.
     """
     queryset = Sheet.objects.all().order_by('-id')
     serializer_class = SheetAdminSerializer
     permission_classes = [IsAdministratorOrInstructor]
     parser_classes = [MultiPartParser, FormParser, JSONParser]
     lookup_field = 'slug'
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'description', 'sheet_type']
 
     def perform_create(self, serializer):
         # Automatically set the currently logged in user as the creator
